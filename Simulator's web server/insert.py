2c974e80f2bd9492c90772b107008e9834d205b4
MarceoGuy <marceau@lumapps.com>
2020-01-08 16:24:01 +0100

    insh

--- Simulator's web server/insert.py
+++ Simulator's web server/insert.py
@@ -6,5 +6,5 @@ for i in range (6):
     for j in range (10):
         #j en x
         #i en y
-        string  = "INSERT INTO public.\"feuSimulated\"(id, x, y, intensity) VALUES ("+ str(10*i+j+1) +", "+ str(j) +", "+ str(i) +", 0);"
+        string  = "INSERT INTO public.capteur(id, x, y, intensity) VALUES ("+ str(10*i+j+1) +", "+ str(j) +", "+ str(i) +", 0);"
         print(string)
